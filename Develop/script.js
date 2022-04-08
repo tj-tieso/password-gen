491ca2f2908e9391c85f484134342d565bb5d4b9
tj-tieso <tj.tieso@gmail.com>
2022-04-08 13:22:08 -0500

    generatepassword() prompts added

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,3 @@
-// Assignment code here
-
 // A. When User clicks "Generate password" they presented with a series of prompts
 //    1. Length of password (8 to 128 characters), then...
 //       - store data
@@ -14,46 +12,58 @@
 // B. Validate Input. At least one character type should be selected.
 // C. Display generated password
 
-//function to generate random password
-// // var
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var includeLowers = String.fromCharCode(97, 122);
+var includeUppers = String.fromCharCode(65, 90);
+var includeNumbers = String.fromCharCode(48, 57);
+var includeSpecials = String.fromCharCode(33, 47).concat();
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); //"password goes here";
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-// var generatePassword () {
-
-// }
-
 // function to select Characters
 var generatePassword = function () {
   // how many characters?
-  var characterSelect = window.prompt(
+  var promptCharacterLength = window.prompt(
     "How many Characters would you like to include? Please enter a number from 8-128."
   );
 
   if (
-    characterSelect === "" ||
-    characterSelect === null ||
-    characterSelect < 8 ||
-    characterSelect > 128
+    promptCharacterLength === "" ||
+    promptCharacterLength === null ||
+    promptCharacterLength < 8 ||
+    promptCharacterLength > 128
   ) {
     window.alert("Please enter a valid answer. Please try again");
     // return to start of function
     return generatePassword();
   }
+  console.log(promptCharacterLength);
+
   // include lowercase?
+  var promptLower = window.prompt(
+    "Would you like to include lowercase characters?"
+  );
+
   // inlude uppercase?
+  var promptUpper = window.prompt(
+    "Would you like to include uppercase characters?"
+  );
+
   // include numbers?
+  var promptNumber = window.prompt("Would you like to include numbers?");
+
   // include special?
-  return "Pasword goes here";
+  var promptSpecial = window.prompt(
+    "Would you like to include special characters?"
+  );
 };
 
 // Add event listener to generate button
