f041c4672c05c4b20802be7f5008a257d0c7036d
tj-tieso <tj.tieso@gmail.com>
2022-04-10 14:48:28 -0500

    character selection added

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,30 @@
-// A. When User clicks "Generate password" they presented with a series of prompts
-//    1. Length of password (8 to 128 characters), then...
-//       - store data
-//    2. Include Lowercase?
-//       - store data
-//    3. Include Uppercase?
-//       - store data
-//    4. Include Numbers?
-//       - store data
-//    5. Include Special?
-//       - store data
-// B. Validate Input. At least one character type should be selected.
-// C. Display generated password
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var includeLowers = String.fromCharCode(97, 122);
-var includeUppers = String.fromCharCode(65, 90);
-var includeNumbers = String.fromCharCode(48, 57);
-var includeSpecials = String.fromCharCode(33, 47).concat();
+// characters, strings from ASCII
+var lowerCharacters = "abcdefghijklmnopqrstuvwxyz"; // String.fromCharCode(97, 122);
+var upperCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // String.fromCharCode(65, 90);
+var numberCharacters = "0123456789"; //String.fromCharCode(48, 57);
+var specialCharacters = "!@#$%^&*_-+=";
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(); //"password goes here";
+  var password = characterSelect(); // should be generatePassword()
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-// function to select Characters
-var generatePassword = function () {
-  // how many characters?
+//Generate password prompts user to select Characters
+function generatePassword() {}
+
+// character selection
+var characterSelect = function () {
+  let characterAmount = "";
+  let characters = "";
   var promptCharacterLength = window.prompt(
     "How many Characters would you like to include? Please enter a number from 8-128."
   );
-
   if (
     promptCharacterLength === "" ||
     promptCharacterLength === null ||
@@ -42,28 +32,40 @@ var generatePassword = function () {
     promptCharacterLength > 128
   ) {
     window.alert("Please enter a valid answer. Please try again");
-    // return to start of function
-    return generatePassword();
-  }
-  console.log(promptCharacterLength);
+    return characterSelect();
+  } else characterAmount = promptCharacterLength;
+  console.log(characterAmount);
 
-  // include lowercase?
-  var promptLower = window.prompt(
-    "Would you like to include lowercase characters?"
+  var promptLowercases = window.confirm(
+    "would you like to include lower case letters?"
   );
+  if (promptLowercases) {
+    (characters += lowerCharacters),
+      console.log("we're including lower case!", characters);
+  } else (characters = ""), console.log("we're not including lowercase");
 
-  // inlude uppercase?
-  var promptUpper = window.prompt(
-    "Would you like to include uppercase characters?"
+  var promptUppercases = window.confirm(
+    "would you like to include uppercase letters?"
   );
+  if (promptUppercases) {
+    (characters += upperCharacters),
+      console.log("we're including uppercase!", characters);
+  } else console.log("we're not including uppercase", characters);
 
-  // include numbers?
-  var promptNumber = window.prompt("Would you like to include numbers?");
+  var promptNumbers = window.confirm("would you like to include numbers?");
+  if (promptNumbers) {
+    (characters += numberCharacters),
+      console.log("we're including numbers!", characters);
+  } else console.log("we're not including uppercase", characters);
 
-  // include special?
-  var promptSpecial = window.prompt(
-    "Would you like to include special characters?"
+  var promptSpecials = window.confirm(
+    "would you like to include special characters?"
   );
+  if (promptSpecials) {
+    (characters += specialCharacters),
+      console.log("we're including numbers!", characters);
+  } else console.log("we're not including uppercase", characters);
+  return characterAmount, characters;
 };
 
 // Add event listener to generate button
